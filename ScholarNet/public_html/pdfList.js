064f2c57f5dbe6ed8fa703f71335de21cccc29e1
NEO Bobokwe <ST10214697@RCCONNECT.EDU.ZA>
2024-10-08 22:01:05 +0200

    First commit (Update)

---
+++ ScholarNet/public_html/pdfList.js
@@ -0,0 +1,43 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+const pdfFiles = [
+    "Grade 10 Accounting P1 (English) 2023 Memo.pdf",
+    "Grade 10 Accounting P1 (English) 2023.pdf",
+    "Grade 10 Accounting P2 (English) November 2022 Memo.pdf",
+    "Grade 10 Accounting P2 (English) November 2022.pdf",
+    "Grade 10 MATH P1 (ENG) June 2022 Memo.pdf",
+    "Grade 10 MATH P1 (ENG) June 2022.pdf",
+    "Grade 10 MATH P2 (ENG & AFR) June 2022 Memo.pdf",
+    "Grade 10 MATH P2 (ENG) June 2022.pdf",
+    "Grade 10 PS P1 (English) November 2022 Memo.pdf",
+    "Grade 10 PS P1 (English) November 2022.pdf",
+    "Grade 10 PS P2 (English)  2023 Memo.pdf",
+    "Grade 10 PS P2 (English)  2023.pdf",
+    "Grade 8 English FAL P2 2017.pdf",
+    "Grade 8 English HL P1 November 2017 Memo.pdf",
+    "Grade 8 English HL P1 November 2017 nov.pdf",
+    "Grade 8 English HL P2 June 2017 Memo.pdf",
+    "Grade 8 English HL P2 June 2017.pdf",
+    "Grade 8 Geo  June 2019 Memo.pdf",
+    "Grade 8 Geo  June 2019.pdf",
+    "Grade 8 Geo November 2016 Memo.pdf",
+    "Grade 8 Geo November 2016.pdf",
+    "Grade 8 Maths June 2019 Memo.pdf",
+    "Grade 8 Maths June 2019.pdf",
+    "Grade 9 MATH (ENG & AFR) June 2022 Memo.pdf",
+    "Grade 9 MATH (ENG) June 2022.pdf",
+    "Grade 9 Math P1 (Eng and Afr) November 2022 Memo.pdf",
+    "Grade 9 Math P1 (Eng) November 2022.",
+    "Grade 9 November Eng P2 2018 EC.pdf",
+    "Grade 9 November Eng P2 2018 Memo EC.pdf",
+    "Grade 9 November Eng P2 2019 EC.pdf",
+    "Grade 9 November Eng P2 2019 Memo EC.pdf",
+    "Grade 9 NS (ENG) June 2022 Memo.pdf",
+    "Grade 9 NS (ENG) June 2022.pdf",
+    "Grade 9 NS (English) November 2022 Memo.pdf",
+    "Grade 9 NS (English) November 2022.pdf"
+];
+
